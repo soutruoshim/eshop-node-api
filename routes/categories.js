0035a61a6861b91eb0b8c57723d22b73d5438c2d
sout_rahim <sout.rahim@gmail.com>
2021-10-06 16:58:10 +0700

    thirth

--- routes/categories.js
+++ routes/categories.js
@@ -8,7 +8,63 @@ router.get(`/`, async (req, res) =>{
     if(!categoryList) {
         res.status(500).json({success: false})
     } 
-    res.send(categoryList);
+    res.status(200).send(categoryList);
 })
 
-module.exports =router;
+router.get(`/:id`, async(req, res) => {
+     const category = await Category.findById(req.params.id);
+     if(!category) {
+        res.status(500).json({success: false})
+    } 
+    res.status(200).send(category);
+});
+
+router.post(`/`, async(req, res) => {
+    let category = new Category({
+        name: req.body.name,
+        icon: req.body.icon,
+        color: req.body.color
+    });
+    category = await category.save();
+    if(!category)
+        return res.status(404).send("The category cannot be created");
+    res.send(category);
+})
+
+router.put(`/:id`, async (req, res) => {
+    const category = await Category.findByIdAndUpdate(
+        req.params.id, {
+            name: req.body.name,
+            icon: req.body.icon,
+            color: req.body.color
+        },
+        {new: true}
+        );
+    if(!category)
+       return res.status(404).send("The category cannot be update");
+    res.send(category);
+});
+
+// api/v1/categories/123
+router.delete(`/:id`, (req, res) => {
+      Category.findByIdAndDelete(req.params.id).then(category=>{
+          if(category){
+              return res.status(200).json({
+                  success: true,
+                  message: 'The category is deleted'
+              })
+          }else{
+              return res.status(404).json({
+                  success: false,
+                  message: 'category not found'
+              })
+          }
+      }).catch(err=>{
+          return res.status(400).json({
+              success: false,
+              error: err
+          })
+      })
+});
+
+module.exports = router;
